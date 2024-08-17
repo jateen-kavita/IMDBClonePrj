4266bb387d6bba650f301cbe6f977add3c4d8e5b
Jateen_Kavita <jateenpatil2002@gmail.com>
2024-08-17 19:25:40 +0530

    updated search function with add to favourates and film section with instant update now doesnt need to refresh to udpate the list fav

--- index.js
+++ index.js
@@ -7,6 +7,81 @@ const Movies = async (movieName) => {
   return MovieAPI;
 };
 
+// Function to handle the search
+
+// Search Funtion
+
+// Search Function
+const searchInputBar = document.querySelector(".searchBox");
+const searchOutput = document.querySelector(".searchResults");
+const SearchBtn = document.getElementById("searchBtn");
+
+// Handle the search process
+const handleSearch = async (searchInput) => {
+  const searchMovies = await Movies(searchInput);
+  console.log("searchMovies:", searchMovies); // Log search results
+
+  const searchedResults = document.createElement("div");
+  searchOutput.innerHTML = "";
+
+  if (searchMovies.Response === "True") {
+    searchedResults.classList.add("inputedsearchResults");
+    searchedResults.innerHTML = `
+      <div class="searchImg">
+        <a href="./Results/result.html" class="movieLink" data-movie="${searchMovies.Title}">
+          <img src="${searchMovies.Poster}" alt="${searchMovies.Title}">
+        </a>
+      </div>
+      <div class="movie-Details">
+        <ul>
+          <li><a href="./Results/result.html" class="movieLink" data-movie="${searchMovies.Title}">Title: ${searchMovies.Title}</a></li>
+          <li>Genre: ${searchMovies.Genre}</li>
+          <li>Cast: ${searchMovies.Actors}</li>
+          <li>Year: ${searchMovies.Year}</li>
+        </ul>
+        <button class="addToFavBtn">Add to Favorites</button>
+      </div>
+    `;
+    searchOutput.appendChild(searchedResults);
+
+    // Add event listener for "Add to Favorites"
+    const addToFavBtn = searchedResults.querySelector(".addToFavBtn");
+    if (addToFavBtn) {
+      addToFavBtn.addEventListener("click", () => {
+        const newFavoriteMovie = {
+          title: searchMovies.Title,
+          poster: searchMovies.Poster,
+          plot: searchMovies.Plot,
+          genre: searchMovies.Genre,
+          imdb: searchMovies.imdbRating,
+        };
+        addMovieToFavorites(newFavoriteMovie);
+      });
+    }
+  } else {
+    searchedResults.innerHTML = `<p>No results found</p>`;
+    searchOutput.appendChild(searchedResults);
+  }
+};
+
+// Function to handle the search input and button
+const searchFunction = () => {
+  searchInputBar.addEventListener("input", async () => {
+    const searchInput = searchInputBar.value;
+    await handleSearch(searchInput);
+  });
+
+  SearchBtn.addEventListener("click", async () => {
+    const searchInput = searchInputBar.value;
+    if (searchInput !== "") {
+      await handleSearch(searchInput);
+    }
+  });
+};
+
+// Call the search function
+searchFunction();
+
 let carouselArr = [
   "Premalu",
   "Godzilla x Kong: The New Empire",
@@ -54,225 +129,6 @@ let popularMovies = [
   "Rockstar",
   "animal",
 ];
-
-// Latest Movies function
-const movieContainer = document.querySelector(".latest-release-container");
-let FavourateMovies = []; // Initialize as an empty array
-
-// Load favorite movies from localStorage on page load
-document.addEventListener("DOMContentLoaded", () => {
-  const storedMovies = localStorage.getItem("favouriteMovies");
-  if (storedMovies) {
-    FavourateMovies = JSON.parse(storedMovies); // Populate the array with stored data
-    renderFavoriteMovies(); // Render the favorite movies on page load
-  }
-});
-
-const film = async (movieName) => {
-  const movieData = await Movies(movieName);
-
-  const movieElement = document.createElement("div");
-  movieElement.classList.add("movie");
-  movieElement.innerHTML = `
-    <a href="https://www.imdb.com/" target="_blank">
-      <img src="${movieData.Poster}" alt="${movieData.Title}" title="${movieData.Title}">
-    </a>
-    <div class="fav-icon">
-      <i class="fa-solid fa-heart heartIcon"></i>
-    </div>
-  `;
-
-  movieContainer.appendChild(movieElement);
-
-  const favIcon = movieElement.querySelector(".fav-icon");
-
-  movieElement.addEventListener("mouseenter", () => {
-    favIcon.style.color = "red";
-    favIcon.style.display = "block";
-  });
-
-  movieElement.addEventListener("mouseleave", () => {
-    favIcon.style.color = "transparent";
-  });
-
-  favIcon.addEventListener("click", () => {
-    const newFavoriteMovie = {
-      title: movieData.Title,
-      poster: movieData.Poster,
-      plot: movieData.Plot,
-      genre: movieData.Genre,
-      imdb: movieData.imdbRating,
-    };
-
-    FavourateMovies.push(newFavoriteMovie);
-
-    // Save to localStorage
-    localStorage.setItem("favouriteMovies", JSON.stringify(FavourateMovies));
-
-    // Immediately re-render the favorite movies list after adding the new movie
-    renderFavoriteMovies();
-  });
-};
-
-function renderFavoriteMovies() {
-  const favouratewrapper = document.querySelector(".fav-container");
-  favouratewrapper.innerHTML = ""; // Clear the container
-
-  FavourateMovies.forEach((movie, index) => {
-    const favMovies = document.createElement("div");
-    favMovies.classList.add("fav-films");
-    favMovies.innerHTML = `
-      <div class="favourateMovieContainer">
-        <div class="image-container">
-          <img src="${movie.poster}" alt="${movie.title}" title="${movie.title}" />
-        </div>
-        <div class="movieDetails">
-          <h4>${movie.title}</h4>
-          <p><span>Storyline:</span> ${movie.plot}</p>
-          <p><span>Movie Genre:&nbsp;</span> ${movie.genre} &nbsp; &nbsp; <span>IMDB Ratings: </span> ${movie.imdb}</p>
-          <div class="watchtrash">
-            <a href="https://www.imdb.com/" target="_blank"><button class="favCTABtn">Watch Now</button></a>
-            <i class="fa-solid fa-trash-can trashcan"></i>
-          </div>
-        </div>
-      </div>`;
-    favouratewrapper.appendChild(favMovies);
-
-    // Attach event listener to remove movie from favorites
-    const trashcan = favMovies.querySelector(".trashcan");
-    trashcan.addEventListener("click", () => {
-      // Remove the movie from the array
-      FavourateMovies.splice(index, 1);
-
-      // Update localStorage
-      localStorage.setItem("favouriteMovies", JSON.stringify(FavourateMovies));
-
-      // Re-render the favorite movies list
-      renderFavoriteMovies();
-    });
-  });
-}
-document.addEventListener("DOMContentLoaded", () => {
-  const storedMovies = localStorage.getItem("favouriteMovies");
-
-  if (storedMovies) {
-    FavourateMoviesArr = JSON.parse(storedMovies);
-    renderFavoriteMovies();
-  }
-});
-
-// This function can be used to populate the movies on load
-const movieDisplay = movieArr.map((movie) => {
-  return film(movie);
-});
-
-
-// Most Popular Section
-
-const popularSection = document.querySelector(".most-popular-container");
-let PopularFavourateMovies = []; // Initialize as an empty array
-
-// Load favorite movies from localStorage on page load
-document.addEventListener("DOMContentLoaded", () => {
-  const storedMovies = localStorage.getItem("favouriteMovies");
-
-  if (storedMovies) {
-    PopularFavourateMovies = JSON.parse(storedMovies); // Populate the array with stored data
-    renderFavoriteMovies(); // Render the favorite movies on page load
-  }
-});
-
-const popularFilms = async (movieName) => {
-  const movieData = await Movies(movieName);
-  const movieContainer = document.createElement("div");
-  movieContainer.classList.add("movie" ,"popularmoviesection");
-  movieContainer.innerHTML = `
-   <a href="https://www.imdb.com/" target="_blank">
-      <img src="${movieData.Poster}" alt="${movieData.Title}" title="${movieData.Title}">
-    </a>
-      <div class="fav-icon" >
-      <i class="fa-solid fa-heart heartIcon" ></i>
-    </div>`;
-  popularSection.appendChild(movieContainer);
-
-  const favIcon = movieContainer.querySelector(".fav-icon");
-
-  movieContainer.addEventListener("mouseenter", () => {
-    favIcon.style.color = "red";
-    favIcon.style.display = "block";
-  });
-
-  movieContainer.addEventListener("mouseleave", () => {
-    favIcon.style.color = "transparent";
-  });
-
-  favIcon.addEventListener("click", () => {
-    const newFavoriteMovie = {
-      title: movieData.Title,
-      poster: movieData.Poster,
-      plot: movieData.Plot,
-      genre: movieData.Genre,
-      imdb: movieData.imdbRating,
-    };
-
-    PopularFavourateMovies.push(newFavoriteMovie);
-
-    // Save to localStorage
-    localStorage.setItem(
-      "favouriteMovies",
-      JSON.stringify(PopularFavourateMovies)
-    );
-
-    renderFavoriteMovies(); // Render the updated list after adding the new movie
-  });
-};
-
-function renderFavoriteMovies() {
-  const favouratewrapper = document.querySelector(".fav-container");
-  favouratewrapper.innerHTML = "";
-
-  PopularFavourateMovies.forEach((movie, index) => {
-    const favMovies = document.createElement("div");
-    favMovies.classList.add("fav-films");
-    favMovies.innerHTML = `
-      <div class="favourateMovieContainer">
-        <div class="image-container">
-          <img src="${movie.poster}" alt="${movie.title}" title="${movie.title}" />
-        </div>
-        <div class="movieDetails">
-          <h4>${movie.title}</h4>
-          <p><span>Storyline:</span> ${movie.plot}</p>
-          <p><span>Movie Genre:</span> ${movie.genre} &nbsp; &nbsp; <span>IMDB Ratings:</span> ${movie.imdb}</p>
-          <div class="watchtrash">
-            <a href="https://www.imdb.com/" target="_blank"><button class="favCTABtn">Watch Now</button></a>
-            <i class="fa-solid fa-trash-can trashcan"></i>
-          </div>
-        </div>
-      </div>`;
-    favouratewrapper.appendChild(favMovies);
-
-    // Attach event listener to remove movie from favorites
-    const trashcan = favMovies.querySelector(".trashcan");
-    trashcan.addEventListener("click", () => {
-      // Remove the movie from the array
-      PopularFavourateMovies.splice(index, 1);
-
-      // Update localStorage
-      localStorage.setItem(
-        "favouriteMovies",
-        JSON.stringify(PopularFavourateMovies)
-      );
-
-      // Re-render the favorite movies list
-      renderFavoriteMovies();
-    });
-  });
-}
-
-// This function can be used to populate the popular films
-const popularMoviesSection = popularMovies.map((movie) => {
-  return popularFilms(movie);
-});
 
 // Carousel Function
 
@@ -332,143 +188,44 @@ carouselMovieShow(carouselArr[currentMovieIndex]);
 // Automatically change the movie every 10 seconds
 const carouselInterval = setInterval(showNextMovie, 8000);
 
-// Function to handle the search
-
-// Search Funtion
-
-const searchInputBar = document.querySelector(".searchBox");
-const searchOutput = document.querySelector(".searchResults");
-const SearchBtn = document.getElementById("searchBtn");
-
-// Search Function
-const searchFunction = () => {
-  searchInputBar.addEventListener("input", async () => {
-    const searchInput = searchInputBar.value;
-    await handleSearch(searchInput);
-  });
-
-  // Trigger search when the search button is clicked
-  SearchBtn.addEventListener("click", async () => {
-    const searchInput = searchInputBar.value;
-    if (searchInput !== "") {
-      await handleSearch(searchInput);
-      
-      // Store the selected movie title in localStorage
-      const searchMovies = await Movies(searchInput);
-      localStorage.setItem("selectedMovie", searchMovies.Title);
-  
-      // Redirect to result.html
-      window.location.href = "./Results/result.html";
-    }
-  });
-  
-
-// Handle the search process
-const handleSearch = async (searchInput) => {
-  const searchMovies = await Movies(searchInput);
-
-  const searchedResults = document.createElement("div");
-  searchOutput.innerHTML = "";
-
-  if (searchInput !== "") {
-    searchedResults.innerHTML = "";
-    searchedResults.classList.add("inputedsearchResults");
-    searchedResults.innerHTML = `
-      <div class="searchImg">
-        <a href="./Results/result.html" class="movieLink" data-movie="${searchMovies.Title}">
-          <img src="${searchMovies.Poster}" alt="${searchMovies.Title}">
-        </a>
-      </div>
-      <div class="movie-Details">
-        <ul>
-          <li><a href="./Results/result.html" class="movieLink" data-movie="${searchMovies.Title}">Title: ${searchMovies.Title}</a></li>
-          <li>Genre: ${searchMovies.Genre}</li>
-          <li>Cast: ${searchMovies.Actors}</li>
-          <li>Year: ${searchMovies.Year}</li>
-        </ul>
-      </div>
-    `;
-
-    // Add click event listener to save the movie title
-    searchedResults.querySelectorAll(".movieLink").forEach((link) => {
-      link.addEventListener("click", (e) => {
-        const movieTitle = e.currentTarget.getAttribute("data-movie");
-        localStorage.setItem("selectedMovie", movieTitle); // Store the movie title
-      });
-    });
-  } else {
-    console.log("Not found");
-  }
-  searchOutput.appendChild(searchedResults);
-}};
-
-// Call the search function
-searchFunction();
 
 
-// Webshow section
 
-const webshowContainer = document.querySelector(".webshow-container");
-let FavourateMoviesArr = []; // Initialize as an empty array
 
+
+// Latest Movies function
+
+
+
+// Common array to store all favorite movies
+let FavourateMovies = [];
+
+// Load favorite movies from localStorage on page load
 document.addEventListener("DOMContentLoaded", () => {
   const storedMovies = localStorage.getItem("favouriteMovies");
-
   if (storedMovies) {
-    FavourateMoviesArr = JSON.parse(storedMovies);
-    renderFavoriteMovies();
+    FavourateMovies = JSON.parse(storedMovies);
+    renderFavoriteMovies(); // Render all favorite movies on page load
   }
 });
-const webshow = async (movieName) => {
-  const movieData = await Movies(movieName);
 
-  const movieElement = document.createElement("div");
-  movieElement.classList.add("movie");
-  movieElement.innerHTML = `
-    <a href="https://www.imdb.com/" target="_blank">
-      <img src="${movieData.Poster}" alt="${movieData.Title}" title="${movieData.Title}">
-    </a>
-    <div class="fav-icon">
-      <i class="fa-solid fa-heart heartIcon"></i>
-    </div>
-  `;
+// Function to update localStorage and re-render the favorites
+function addMovieToFavorites(newFavoriteMovie) {
+  FavourateMovies.push(newFavoriteMovie);
 
-  webshowContainer.appendChild(movieElement);
+  // Save to localStorage
+  localStorage.setItem("favouriteMovies", JSON.stringify(FavourateMovies));
 
-  const favIcon = movieElement.querySelector(".fav-icon");
+  // Re-render the favorite movies list
+  renderFavoriteMovies();
+}
 
-  movieElement.addEventListener("mouseenter", () => {
-    favIcon.style.color = "red";
-    favIcon.style.display = "block";
-  });
-
-  movieElement.addEventListener("mouseleave", () => {
-    favIcon.style.color = "transparent";
-  });
-
-  favIcon.addEventListener("click", () => {
-    const newFavoriteMovie = {
-      title: movieData.Title,
-      poster: movieData.Poster,
-      plot: movieData.Plot,
-      genre: movieData.Genre,
-      imdb: movieData.imdbRating,
-    };
-
-    FavourateMoviesArr.push(newFavoriteMovie);
-
-    // Save to localStorage
-    localStorage.setItem("favouriteMovies", JSON.stringify(FavourateMoviesArr));
-
-    renderFavoriteMovies();
-  });
-};
-
+// Common function to render the favorites list
 function renderFavoriteMovies() {
   const favouratewrapper = document.querySelector(".fav-container");
   favouratewrapper.innerHTML = "";
 
-  FavourateMoviesArr.forEach((movie, index) => {
+  FavourateMovies.forEach((movie, index) => {
     const favMovies = document.createElement("div");
     favMovies.classList.add("fav-films");
     favMovies.innerHTML = `
@@ -492,13 +249,10 @@ function renderFavoriteMovies() {
     const trashcan = favMovies.querySelector(".trashcan");
     trashcan.addEventListener("click", () => {
       // Remove the movie from the array
-      FavourateMoviesArr.splice(index, 1);
+      FavourateMovies.splice(index, 1);
 
       // Update localStorage
-      localStorage.setItem(
-        "favouriteMovies",
-        JSON.stringify(FavourateMoviesArr)
-      );
+      localStorage.setItem("favouriteMovies", JSON.stringify(FavourateMovies));
 
       // Re-render the favorite movies list
       renderFavoriteMovies();
@@ -506,9 +260,83 @@ function renderFavoriteMovies() {
   });
 }
 
-// Load favorite movies from localStorage on page load
+// Function to handle movie elements (latest release, webshow, popular)
+function handleMovieElement(movieData, container) {
+  const movieElement = document.createElement("div");
+  movieElement.classList.add("movie");
+  movieElement.innerHTML = `
+    <a href="https://www.imdb.com/" target="_blank">
+      <img src="${movieData.Poster}" alt="${movieData.Title}" title="${movieData.Title}">
+    </a>
+    <div class="fav-icon">
+      <i class="fa-solid fa-heart heartIcon"></i>
+    </div>
+  `;
 
+  container.appendChild(movieElement);
 
-const webshowDisplay = webshowArr.map((movie) => {
-  return webshow(movie);
-});
+  const favIcon = movieElement.querySelector(".fav-icon");
+
+  movieElement.addEventListener("mouseenter", () => {
+    favIcon.style.color = "red";
+    favIcon.style.display = "block";
+  });
+
+  movieElement.addEventListener("mouseleave", () => {
+    favIcon.style.color = "transparent";
+  });
+
+  favIcon.addEventListener("click", () => {
+    const isAlreadyFavorite = FavourateMovies.some(
+      (movie) => movie.title === movieData.Title
+    );
+
+    if (isAlreadyFavorite) {
+      alert("This movie is already in your favorites!");
+    } else {
+      const newFavoriteMovie = {
+        title: movieData.Title,
+        poster: movieData.Poster,
+        plot: movieData.Plot,
+        genre: movieData.Genre,
+        imdb: movieData.imdbRating,
+      };
+
+      addMovieToFavorites(newFavoriteMovie); // Add to favorites using the common function
+    }
+  });
+}
+
+// Latest Release Section
+const movieContainer = document.querySelector(".latest-release-container");
+
+async function film(movieName) {
+  const movieData = await Movies(movieName);
+  handleMovieElement(movieData, movieContainer);
+}
+
+// Webshow Section
+const webshowContainer = document.querySelector(".webshow-container");
+
+async function webshow(movieName) {
+  const movieData = await Movies(movieName);
+  handleMovieElement(movieData, webshowContainer);
+}
+
+// Most Popular Section
+const popularSection = document.querySelector(".most-popular-container");
+
+async function popularFilms(movieName) {
+  const movieData = await Movies(movieName);
+  handleMovieElement(movieData, popularSection);
+}
+
+// Example of populating movies for different sections
+// const movieArr = ["Inception", "Avatar", "Titanic"]; // Replace with actual movie names
+movieArr.forEach((movie) => film(movie));
+
+// const webshowArr = ["Breaking Bad", "Game of Thrones"]; // Replace with actual show names
+webshowArr.forEach((show) => webshow(show));
+
+// const popularMovies = ["The Dark Knight", "Interstellar"]; // Replace with actual movie names
+popularMovies.forEach((movie) => popularFilms(movie));
